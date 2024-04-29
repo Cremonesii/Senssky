6c8438612adbb4200d99f6a292d53a25bcb8e056
LCRoque <lucas.roque@sptech.school>
2024-04-29 20:24:11 -0300

    Comentei animação JS

--- Sprint2/Site/animation.js
+++ Sprint2/Site/animation.js
@@ -1,25 +1,4 @@
-const sidebar = document.querySelector(".sidebar");
-const sidebarClose = document.querySelector("#sidebar-close");
-const menu = document.querySelector(".menu-content");
-const menuItems = document.querySelectorAll(".submenu-item");
-const subMenuTitles = document.querySelectorAll(".submenu .menu-title");
+const sidebar = document.querySelector(".sidebar"); // Esta linha referência para a div que possui a classe "sidebar"
+const sidebarClose = document.querySelector("#sidebar-close"); // Esta linha que possui o id "sidebar-close"
 
-sidebarClose.addEventListener("click", () => sidebar.classList.toggle("close"));
-
-menuItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    menu.classList.add("submenu-active");
-    item.classList.add("show-submenu");
-    menuItems.forEach((item2, index2) => {
-      if (index !== index2) {
-        item2.classList.remove("show-submenu");
-      }
-    });
-  });
-});
-
-subMenuTitles.forEach((title) => {
-  title.addEventListener("click", () => {
-    menu.classList.remove("submenu-active");
-  });
-});
+sidebarClose.addEventListener("click", () => sidebar.classList.toggle("close"));// Esta para o clique no elemento referenciado por sidebarClose, ou seja, é uma animação do JS
