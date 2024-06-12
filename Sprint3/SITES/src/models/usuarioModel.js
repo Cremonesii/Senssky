1f92ddbc0eff5f7b827e8c232b35664a34a78b53
enzo-mmaciel <enzo.maciel@sptech.school>
2024-06-11 21:33:44 -0300

    Criptografando senha do cadastro

--- Sprint3/SITES/src/models/usuarioModel.js
+++ Sprint3/SITES/src/models/usuarioModel.js
@@ -1,22 +1,42 @@
-var database = require("../database/config")
+const crypto = require('crypto');
+var database = require("../database/config");
+
+
+const chaveSecreta = 'chave-secreta-aqui';
+
+
+function criptografar(texto) {
+    const cipher = crypto.createCipher('aes-256-cbc', chaveSecreta);
+    let criptografado = cipher.update(texto, 'utf8', 'hex');
+    criptografado += cipher.final('hex');
+    return criptografado;
+}
+
+
+function descriptografar(criptografado) {
+    const decipher = crypto.createDecipher('aes-256-cbc', chaveSecreta);
+    let descriptografado = decipher.update(criptografado, 'hex', 'utf8');
+    descriptografado += decipher.final('utf8');
+    return descriptografado;
+}
+
 
 function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha);
+    const senhaCriptografada = criptografar(senha); 
     var instrucaoSql = `
-        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senhaCriptografada}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+
 function cadastrar(nome, email, senha, empresaId) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+    const senhaCriptografada = criptografar(senha); 
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${empresaId}');
+        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senhaCriptografada}', '${empresaId}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -24,5 +44,7 @@ function cadastrar(nome, email, senha, empresaId) {
 
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    criptografar,
+    descriptografar
 };
